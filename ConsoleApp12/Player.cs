08fb7ff501a65a7c9775f60d878a435793b972d3
Administrator <Administrator@CHAUHAN>
2022-03-09 11:42:03 +0530

    Add project files.

---
+++ ConsoleApp12/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Player
+    {
+        public string name = "Vikas";               // parameter
+        private int health = 50;
+
+        public int getHealth()       // adding a method with getHealth function(because fun can access its private members) to access private parametre i.e health
+        {
+            return health;
+        }
+
+        public void setHealth(int h)         // adding a Method
+        {
+            health = h;
+        }
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player tommy = new Player();     // create object of class Player
+            Console.WriteLine(tommy.name);        //print parameter by Console.Writeline(object.parameter);
+            //Console.WriteLine(tommy.health);
+
+            //tommy.setHealth(100);                   // call method by [ object.methodName ]
+            //Console.WriteLine(tommy.health);            // print the health
+            
+            
+            Console.WriteLine(tommy.getHealth());
+            tommy.setHealth(100);
+            Console.WriteLine(tommy.getHealth());
+            Console.ReadLine();
+        }
+    }
+}
